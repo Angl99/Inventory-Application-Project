c6e4133a0a5cc9be1a79670f89718ae73b7cf3a1
Angl99 <anggelplasencia@pursuit.org>
2023-10-16 19:05:29 -0400

    starting proj

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const { faker } = require('@faker-js/faker');
+const fs = require("node:fs");
+const { writeFileSync } = require("node:fs");
+
+function generateUser() {
+    
+ console.log(users);
+}
+
